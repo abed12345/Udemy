35245d533e805aa11aac5a4078553bf26024f18e
abed12345 <abedborsa1993@gmail.com>
2022-02-25 15:56:05 -0500

    trying to solve a route path issue

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,15 @@
 class PublicController < ApplicationController
     
     def homepage
+        @posts= Post.all
+    end
+
+    def about
     end
     
+    def contact
+    end
+    
+    def blog
+    end
 end 
--- app/views/posts/_post.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! post, :id, :Title, :summary, :body, :active, :created_at, :updated_at
-json.url post_url(post, format: :json)
