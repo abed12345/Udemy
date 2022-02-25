a6d4203ffa37c9f3a8e855e8cc064f3c1e92cc58
abed12345 <abedborsa1993@gmail.com>
2022-02-25 16:28:10 -0500

    trying to send a local variable to a partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
-  get "about" => "public#about", as: about
-  get "blog" => "public#blog", as: blog
-  get "contact" => "public#contact", as: contact
+  get "about" => "public#about", as: 'about'
+  get "blog" => "public#blog", as: 'blog'
+  get "contact" => "public#contact", as: 'contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     root to: "public#homepage"
   # Defines the root path route ("/")
