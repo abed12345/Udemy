251777f1414ac9daeab4dd56d46e405333b30c0d
abed12345 <abedborsa1993@gmail.com>
2022-02-28 14:10:29 -0500

    adding a helper

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+    validates :Title, presence: true
+    validates :body, presence: true, length: { minimum: 2 }
+    validates :summary, presence: true
+    
+    def details
+        "created on #{created_at.strftime("%d %M %Y")}"
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_24_154557) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_28_154642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "Title"
